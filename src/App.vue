0ad616e43433e3a874390424a262612029830eae
craftycram <github@craftycram.net>
2020-06-18 10:38:31 +0200

    moved from HelloWorld.vue towards DataViz.vue

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!--<img alt="Vue logo" src="./assets/logo.png">-->
+    <DataViz msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import DataViz from './components/DataViz.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    DataViz
   }
 }
 </script>
--- src/components/DataViz.vue
+++ src/components/DataViz.vue
@@ -1,5 +1,8 @@
 <template>
-  
+  <div>
+    <h1>Cyberthreats</h1>
+    <h2>Datenvisualisierung</h2>
+  </div>
 </template>
 
 <script>
