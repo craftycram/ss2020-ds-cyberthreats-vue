7426ce3f08870adb943c1e9577af345752c43168
craftycram <github@craftycram.net>
2020-06-18 11:42:44 +0200

    added attack visualization

--- src/components/DataViz.vue
+++ src/components/DataViz.vue
@@ -2,6 +2,16 @@
   <div>
     <h1>Cyberthreats</h1>
     <h2>Datenvisualisierung</h2>
+    <div>
+      <div
+      class="attack"
+      v-for="attack in attacks"
+      :key="attack.id"
+      :style="{ width: attack.id+'px' }"
+      >
+      {{attack.Origin}}
+    </div>
+    </div>
   </div>
 </template>
 
@@ -17,7 +27,7 @@ export default {
   mounted() {
     axios.get('http://localhost:3000')
       .then(response => {
-        this.attacks = response.data.attacks;
+        this.attacks = response.data;
         console.log(this.attacks[0]);
       })
       .catch(err => {
@@ -28,5 +38,8 @@ export default {
 </script>
 
 <style>
-
+.attack{
+  background-color: grey;
+  display: inline-block
+}
 </style>
