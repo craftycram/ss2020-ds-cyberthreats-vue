53bae8a9df610b9971dc94205001f12adb5d88af
craftycram <github@craftycram.net>
2020-06-18 12:00:27 +0200

    added range sliders

--- src/components/DataViz.vue
+++ src/components/DataViz.vue
@@ -2,10 +2,15 @@
   <div>
     <h1>Cyberthreats</h1>
     <h2>Datenvisualisierung</h2>
-    <div>
+    <div class="controls">
+      {{rangeMin}} / {{rangeMax}} | {{attacks.length}}
+      Minimum <input type="range" min="0" :max="attacks.length" v-model="rangeMin" id="rangeStart">
+      Maximum <input type="range" min="0" :max="attacks.length" v-model="rangeMax" id="rangeEnd">
+    </div>
+    <div class="attacks">
       <div
       class="attack"
-      v-for="attack in attacks"
+      v-for="attack in rangeOfAttacks"
       :key="attack.id"
       :style="{ width: attack.id+'px' }"
       >
@@ -21,7 +26,9 @@ import axios from 'axios'
 export default {
   data: function () {
     return {
-      attacks: undefined
+      attacks: Array,
+      rangeMin: 0,
+      rangeMax: 50,
     }
   },
   mounted() {
@@ -33,6 +40,11 @@ export default {
       .catch(err => {
         console.log(err)
       });
+  },
+  computed: {
+    rangeOfAttacks: function() {
+      return this.attacks.slice(this.rangeMin, this.rangeMax)
+    }
   }
 }
 </script>
@@ -40,6 +52,6 @@ export default {
 <style>
 .attack{
   background-color: grey;
-  display: inline-block
+  /*display: inline-block*/
 }
 </style>
