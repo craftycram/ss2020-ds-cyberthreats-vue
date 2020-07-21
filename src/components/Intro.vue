eb5bbf05ea109155ebb3ee7746b7ce59047c1f26
craftycram <github@craftycram.net>
2020-07-21 19:20:29 +0200

    moved lang switcher

--- src/components/Intro.vue
+++ src/components/Intro.vue
@@ -1,5 +1,14 @@
 <template>
   <div class="main">
+    <b-dropdown variant="secondary">
+      <template v-slot:button-content>
+        <b-icon icon="flag-fill" aria-hidden="true"></b-icon> {{$t('lang')}}
+      </template>
+
+      <b-dropdown-item-button @click="changeLang('de')">Deutsch</b-dropdown-item-button>
+      <b-dropdown-item-button @click="changeLang('en')">English</b-dropdown-item-button>
+    </b-dropdown>
+    
     <b-jumbotron bg-variant="dark" header="Cyberthreats" :lead="$t('lead')">
     <p>
       <b-icon icon="cursor" class="h2 mb-2" shift-v="-6"></b-icon>
@@ -13,14 +22,6 @@
     <br>
     <b-button style="margin-right: 10px;" variant="primary" @click="$emit('start-event')">{{$t('btn-start')}}</b-button>
     <b-button style="margin-right: 70%;" variant="secondary" href="https://projects.hfg-gmuend.de/iot/2020ss-iot2-darstellensimulation/dokumentation-darstellen-simulation-viktoria-galver-marc-rufeis/einfuhrung">{{$t('btn-docu')}}</b-button>
-    <b-dropdown variant="secondary">
-      <template v-slot:button-content>
-        <b-icon icon="flag-fill" aria-hidden="true"></b-icon> {{$t('lang')}}
-      </template>
-
-      <b-dropdown-item-button @click="changeLang('de')">Deutsch</b-dropdown-item-button>
-      <b-dropdown-item-button @click="changeLang('en')">English</b-dropdown-item-button>
-    </b-dropdown>
   </b-jumbotron>
   </div>
 </template>
@@ -43,5 +44,11 @@ export default {
     height: 100vh;
     padding: 100px;
     background-image: url("../assets/canvas-merged.png")
+  }
+
+  .btn-group.dropdown.b-dropdown{
+    position: absolute;
+    right: 20px;
+    top: 20px;
   }
 </style>
